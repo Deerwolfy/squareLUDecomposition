8fa513e2c91af9f4c3baa68be5933e3ba1ae3417
darkatrus <darkatrus@yandex.ru>
2022-04-12 20:55:26 +0100

    Added error handling

--- mat_func.c
+++ mat_func.c
@@ -4,12 +4,13 @@
 
 void mat_print(double** mat, unsigned int n, const char* label)
 {
-  if (mat != NULL) {
-    if (label)
+  if(mat) {
+    if(label){
       printf("%s\n", label);
-    for (unsigned int i = 0; i < n; ++i) {
-      for (unsigned int j = 0; j < n; ++j) {
-        if (mat[i] != NULL) {
+    }
+    for(unsigned int i = 0; i < n; ++i) {
+      for(unsigned int j = 0; j < n; ++j) {
+        if(mat[i]) {
           printf("%8.4f ", mat[i][j]);
         }
       }
@@ -20,14 +21,11 @@ void mat_print(double** mat, unsigned int n, const char* label)
 
 double** mat_generate(unsigned int n)
 {
-  double** mat = (double**) malloc(n*sizeof(double*));
-  if (mat != NULL) {
-    for (unsigned int i = 0; i < n; ++i) {
-      mat[i] = (double*) malloc(n * sizeof(double));
-      if (mat[i] != NULL) {
-        for (unsigned int j = 0; j < n; ++j) {
-          mat[i][j] = (((int)((rand()%100)*1000))/1000);
-        }
+  double** mat = mat_square_alloc(n);
+  if(mat) {
+    for(unsigned int i = 0; i < n; ++i){
+      for(unsigned int j = 0; j < n; ++j){
+        mat[i][j] = rand() % 100;
       }
     }
   }
@@ -37,13 +35,16 @@ double** mat_generate(unsigned int n)
 double** mat_square_alloc(unsigned int n)
 {
   double** mat = (double**)malloc(n * sizeof(double*));
-  if (mat != NULL) {
-    for (unsigned int i = 0; i < n; ++i) {
+  if(mat) {
+    for(unsigned int i = 0; i < n; ++i) {
       mat[i] = (double*)malloc(n * sizeof(double));
-      if (mat[i] != NULL) {
-        for (unsigned int j = 0; j < n; ++j) {
-          mat[i][j] = 0;
+      if(!mat[i]) {
+        while(--i != -1){
+          free(mat[i]);
         }
+        free(mat);
+        mat = NULL;
+        break;
       }
     }
   }
@@ -52,25 +53,33 @@ double** mat_square_alloc(unsigned int n)
 
 void mat_free(double **mat, unsigned int n)
 {
-  if (mat != NULL) {
-    for (unsigned int i = 0; i < n; ++i) {
-      free(mat[i]);
+  if(mat) {
+    for(unsigned int i = 0; i < n; ++i) {
+      if(mat[i]){
+        free(mat[i]);
+      }
     }
+    free(mat);
   }
-  free(mat);
 }
 
 void mat_LU_split(double **LU, double ***L, double ***U, unsigned int n)
 {
-  if (!U || !L || !LU) return;
-  for (unsigned int i = 0; i < n; ++i) {
-    for (unsigned int j = 0; j < n; ++j) {
-      if (j < i) {
+  if(!U || !L || !LU) return;
+  if(!(*L)){
+    *L = mat_square_alloc(n);
+  }
+  if(!(*U)){
+    *U = mat_square_alloc(n);
+  }
+  for(unsigned int i = 0; i < n; ++i) {
+    for(unsigned int j = 0; j < n; ++j) {
+      if(j < i) {
         (*L)[i][j] = LU[i][j];
       }
-      else if (j >= i) {
+      else if(j >= i) {
         (*U)[i][j] = LU[i][j];
-        if (j == i) {
+        if(j == i) {
           (*L)[i][j] = 1;
         }
       }
@@ -80,11 +89,14 @@ void mat_LU_split(double **LU, double ***L, double ***U, unsigned int n)
 
 double** mat_mult(double** l, double** r, unsigned int n)
 {
+  if(!l || !r) return NULL;
   double** result = mat_square_alloc(n);
-  for (unsigned int i = 0; i < n; ++i) {
-    for (unsigned int j = 0; j < n; ++j) {
-      for (unsigned int k = 0; k < n; ++k) {
-        result[i][j] += l[i][k] * r[k][j];
+  if(result){
+    for(unsigned int i = 0; i < n; ++i) {
+      for(unsigned int j = 0; j < n; ++j) {
+        for(unsigned int k = 0; k < n; ++k) {
+          result[i][j] += l[i][k] * r[k][j];
+        }
       }
     }
   }
@@ -98,6 +110,7 @@ int sign(double num)
 
 char mat_cmp(double** r, double** l, unsigned int n)
 {
+  if(!r || !l) return 0;
   for(unsigned int i = 0; i < n; ++i){
     for(unsigned int j = 0; j < n; ++j){
       if(fabs(r[i][j] - l[i][j]) > MAT_EPSILON){
@@ -110,10 +123,13 @@ char mat_cmp(double** r, double** l, unsigned int n)
 
 double** mat_copy(double** mat, unsigned int n)
 {
+  if(!mat) return NULL;
   double** copy = mat_square_alloc(n);
-  for(unsigned int i = 0; i < n; ++i){
-    for(unsigned int j = 0; j < n; ++j){
-      copy[i][j] = mat[i][j];
+  if(copy){
+    for(unsigned int i = 0; i < n; ++i){
+      for(unsigned int j = 0; j < n; ++j){
+        copy[i][j] = mat[i][j];
+      }
     }
   }
   return copy;
